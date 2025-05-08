3f2d8d7f8ed1ea32a09ef2d68926f6616ed2731e
IliasVilux <iliasotsman@gmail.com>
2025-05-08 12:52:00 +0200

    feat(migrations): :card_file_box: Add database migrations
    Add migrations for tables: categories, platforms, contents, favorite_lists, seasons, episodes, ratings, and reviews. Also, added relational pivot tables for many-to-many relationships.

---
+++ database/migrations/2025_05_08_101228_create_content_platform_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('content_platform', function (Blueprint $table) {
+            $table->foreignId('content_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('platform_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+
+            $table->primary(['content_id', 'platform_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('content_platform');
+    }
+};
