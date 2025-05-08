2277bcff8cd3436a6d7ef38470725b45f8f53b8b
IliasVilux <iliasotsman@gmail.com>
2025-05-09 00:05:23 +0200

    feat: :card_file_box: Hydrate database with seeder using factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Content;
+use App\Models\Episode;
+use App\Models\FavoriteList;
+use App\Models\Platform;
+use App\Models\Rating;
+use App\Models\Review;
+use App\Models\Season;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +21,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Category::factory(10)->create();
+        $platforms = Platform::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $movies = Content::factory(8)->create(['type' => 'movie']);
+        $series = Content::factory(8)->create(['type' => 'series']);
+        $animes = Content::factory(8)->create(['type' => 'anime']);
+
+        $allContents = $movies->concat($series)->concat($animes);
+
+        $series->concat($animes)->each(function ($content) {
+            $seasons = Season::factory(rand(1, 5))->create(['content_id' => $content->id]);
+
+            $seasons->each(function ($season) {
+                Episode::factory(rand(1, 10))->create(['season_id' => $season->id]);
+            });
+        });
+
+        $users = User::factory(10)->create();
+
+        foreach ($users as $user) {
+            $lists = FavoriteList::factory(rand(1, 3))->create(['user_id' => $user->id]);
+
+            foreach ($lists as $list)
+            {
+                $randomContents = $allContents->random(rand(3, 6));
+                $list->contents()->attach($randomContents->pluck('id'));
+            }
+
+            foreach ($allContents as $content)
+            {
+                $randomPlatforms = $platforms->random(rand(2, 5));
+                $content->platforms()->syncWithoutDetaching($randomPlatforms->pluck('id'));
+            }
+
+            $ratedContents = $allContents->random(rand(5, 15));
+            foreach ($ratedContents as $content) {
+                Rating::factory()->create([
+                    'user_id' => $user->id,
+                    'content_id' => $content->id,
+                ]);
+
+                if (rand(0, 1)) {
+                    Review::factory()->create([
+                        'user_id' => $user->id,
+                        'content_id' => $content->id,
+                    ]);
+                }
+            }
+        }
     }
 }
