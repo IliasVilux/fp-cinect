b0ae52d0749577d817a8b8ef212dc9efea1e5868
IliasVilux <iliasotsman@gmail.com>
2025-05-21 23:18:58 +0200

    feat: :sparkles: Call 'WelcomeController' in web.php, create 'Welcome' page, models interfaces

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class WelcomeController extends Controller
+{
+    public function index(): Response
+    {
+        $categories = Category::with(['contents' => function ($query) {
+            $query->latest()->limit(1);
+        }])->get();
+
+        $categories = $categories->map(function ($category) {
+            $category->content = $category->contents->first();
+            unset($category->contents);
+            return $category;
+        });
+
+        return Inertia::render('Welcome');
+    }
+}
