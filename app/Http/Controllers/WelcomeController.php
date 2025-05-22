7f1920421816f6baf0c075c63bfc59a7be05126d
IliasVilux <iliasotsman@gmail.com>
2025-05-22 17:45:14 +0200

    feat: :sparkles: Add trending contents in 'WelcomeController' & and real info in view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Content;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -11,9 +13,11 @@ class WelcomeController extends Controller
 {
     public function index(): Response
     {
-        $categories = Category::with(['contents' => function ($query) {
-            $query->latest()->limit(1);
-        }])->get();
+        $categories = Category::with([
+            'contents' => function ($query) {
+                $query->latest()->limit(1);
+            }
+        ])->get();
 
         $categories = $categories->map(function ($category) {
             $category->content = $category->contents->first();
@@ -21,8 +25,27 @@ class WelcomeController extends Controller
             return $category;
         });
 
+        $weekAgo = Carbon::now()->subWeek();
+        $trendingContents = Content::withCount([
+            'reviews' => function ($query) use ($weekAgo) {
+                $query->where('created_at', '>=', $weekAgo);
+            },
+            'ratings' => function ($query) use ($weekAgo) {
+                $query->where('created_at', '>=', $weekAgo);
+            },
+            'favoriteLists' => function ($query) use ($weekAgo) {
+                $query->where('favorite_list_content.created_at', '>=', $weekAgo);
+            },
+        ])->get()->sortByDesc(function ($content) {
+            return
+                ($content->reviews_count * 3) +
+                ($content->ratings_count * 2) +
+                ($content->favorite_lists_count * 1);
+        })->take(5);
+
         return Inertia::render('Welcome', [
-            'categories'=> $categories
+            'categories' => $categories,
+            'trendingContents' => $trendingContents,
         ]);
     }
 }
--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -3,11 +3,35 @@ import WelcomeHeader from '@/components/WelcomeHeader.vue';
 import { Head } from '@inertiajs/vue3';
 import { Card, CardContent } from '@/components/ui/card'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
-import type { Category } from '@/types/models'
+import type { Category, Content } from '@/types/models'
 
 defineProps<{
     categories: Category[]
+    trendingContents: Content[]
 }>()
+
+const features = [
+    {
+        title: 'Descubre nuevo contenido',
+        description: 'Explora películas, series y animes recomendados para ti según tus gustos.',
+        icon: ''
+    },
+    {
+        title: 'Guarda tu contenido según tus preferencias',
+        description: 'Crea listas personalizadas y accede a tu contenido favorito fácilmente.',
+        icon: ''
+    },
+    {
+        title: 'Vota y haz reviews',
+        description: 'Comparte tu opinión y vota el contenido que más te gusta.',
+        icon: ''
+    },
+    {
+        title: 'Tu catálogo, a tu manera',
+        description: 'Organiza, filtra y descubre nuevo contenido como nunca antes.',
+        icon: ''
+    }
+]
 </script>
 
 <template>
@@ -34,7 +58,7 @@ defineProps<{
         </div>
     </section>
 
-    <section class="w-full px-2 lg:px-4 overflow-x-hidden mb-16">
+    <section class="w-full px-2 lg:px-4 overflow-x-hidden mb-5">
         <h2 class="text-2xl lg:text-3xl lg:text-center font-medium lg:font-bold mb-4">Entretenimiento incomparable</h2>
 
         <Carousel class="relative w-full" :opts="{
@@ -62,23 +86,25 @@ defineProps<{
         </Carousel>
     </section>
 
-    <h2 class="text-2xl lg:text-3xl lg:text-center font-medium lg:font-bold mb-4">Entretenimiento incomparable</h2>
-    <section class="w-full min-h-[400px] lg:min-h-[600px] relative text-center md:text-start lg:flex items-end overflow-hidden mb-16">
+    <section
+        class="w-full min-h-[400px] lg:min-h-[600px] relative lg:flex items-end overflow-hidden mb-16">
         <!-- Imagen de fondo -->
-        <img src="/images/welcome/hero-background.webp" alt="Fondo"
+        <img v-if="trendingContents.length && trendingContents[0].cover_image" src="/images/welcome/hero-background.webp" :alt="trendingContents[0].title"
             class="lg:absolute inset-0 w-full h-full object-cover z-0 mask-b-from-20% lg:mask-b-from-70% lg:mask-b-to-95% lg:mask-x-from-65%" />
 
         <!-- Contenido -->
         <div class="z-10 max-w-4xl mx-auto -mt-20 lg:my-0 size-full px-2 lg:px-4">
             <!-- Textos -->
-            <div class="mb-4">
-                <h1 class="text-2xl font-black uppercase space">Título principal</h1>
-                <p class="max-w-2xl text-7xl font-extralight">Subtítulo o descripción</p>
+            <div class="relative mb-4">
+                <h1 class="text-2xl font-black uppercase space text-center md:text-start">Tendencias</h1>
+                <p class="max-w-2xl text-4xl md:text-6xl lg:text-7xl font-extralight">Lo que todos están viendo ahora mismo</p>
             </div>
 
             <!-- Grid -->
             <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
-                <div v-for="i in 4" :key="i" class="bg-indigo-500 rounded-xl overflow-hidden aspect-square"></div>
+                <img v-for="content in trendingContents.slice(1)" :key="content.id"
+                    src="/images/welcome/hero-background.webp" :alt="content.title"
+                    class="object-cover size-full rounded-xl overflow-hidden aspect-square">
             </div>
         </div>
     </section>
@@ -86,12 +112,13 @@ defineProps<{
     <section class="w-full px-2 lg:px-4 mb-16">
         <h2 class="text-2xl lg:text-3xl lg:text-center font-medium lg:font-bold mb-4">Más motivos para unirte</h2>
         <div class="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-4 gap-2">
-            <div v-for="i in 4" :key="i" class="rounded-xl border border-neutral-800 p-4">
+            <div v-for="(feature, i) in features" :key="i"
+                class="bg-neutral-900 rounded-xl border border-neutral-800 p-4 h-full flex flex-col justify-between">
                 <div class="mb-6">
-                    <p class="text-lg font-bold mb-4">Lorem, ipsum dolor.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, quas?</p>
+                    <p class="text-lg font-bold mb-2">{{ feature.title }}</p>
+                    <p class="text-neutral-300">{{ feature.description }}</p>
                 </div>
-                <div class="size-8 bg-indigo-500 ml-auto"></div>
+                <div class="size-8 bg-indigo-500 ml-auto">{{ feature.icon }}</div>
             </div>
         </div>
     </section>
