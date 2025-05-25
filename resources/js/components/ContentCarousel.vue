dd3a11dc0d37192b1d85afc60b277f0eeaaa8bf1
IliasVilux <iliasotsman@gmail.com>
2025-05-25 20:52:48 +0200

    feat: :sparkles: Add ContentCarousel component

--- resources/js/components/ContentCarousel.vue
+++ resources/js/components/ContentCarousel.vue
@@ -9,32 +9,26 @@ defineProps<{
 </script>
 
 <template>
-    <Carousel class="relative w-full" :opts="{
-            align: 'center',
-        }" v-slot="{ canScrollPrev, canScrollNext }">
-            <div class="absolute z-10 w-24 h-full bg-gradient-to-r from-[#101010]/100 to-[#101010]/0 opacity-0 transition-opacity duration-300" :class="{'md:opacity-100' : canScrollPrev}"></div>
-            <div class="absolute right-0 z-10 w-24 h-full bg-gradient-to-l from-[#101010]/100 to-[#101010]/0 opacity-0 transition-opacity duration-300" :class="{'md:opacity-100' : canScrollNext}"></div>
+  <Carousel class="relative w-full" :opts="{ align: 'center' }" v-slot="{ canScrollPrev, canScrollNext }">
+    <div class="absolute z-10 w-24 h-full bg-gradient-to-r from-[#101010]/100 to-[#101010]/0 opacity-0 transition-opacity duration-300" :class="{'md:opacity-100' : canScrollPrev}"></div>
+    <div class="absolute right-0 z-10 w-24 h-full bg-gradient-to-l from-[#101010]/100 to-[#101010]/0 opacity-0 transition-opacity duration-300" :class="{'md:opacity-100' : canScrollNext}"></div>
 
-            <CarouselContent class="-ml-2">
-                <CarouselItem v-for="(content, index) in contents" :key="content.id"
-                    class="basis-1/2 md:basis-1/4 lg:basis-1/6 pl-2">
-                    <Card class="relative rounded-lg overflow-hidden aspect-[5/8]">
-                        <CardContent>
-                            <!-- Imagen de fondo -->
-                            <img :src="content.cover_image || '/images/welcome/hero-background.webp'"
-                                :alt="`${content.title} cover image`"
-                                class="absolute inset-0 size-full object-cover z-0 mask-b-from-70% mask-b-to-95%" />
+    <CarouselContent class="-ml-2">
+      <CarouselItem v-for="content in contents" :key="content.id" class="basis-1/2 md:basis-1/4 lg:basis-1/6 pl-2">
+        <Card class="relative rounded-lg overflow-hidden aspect-[5/8]">
+          <CardContent>
+            <img :src="content.cover_image || '/images/welcome/hero-background.webp'" :alt="`${content.title} cover image`"
+              class="absolute inset-0 size-full object-cover z-0 mask-b-from-70% mask-b-to-95%" />
 
-                            <!-- Contenido -->
-                            <div class="absolute z-10 mb-4 bottom-0 left-0 w-full text-center">
-                                <slot name="item-extra" :item="content" :index="index" />
-                                <p class="text-[10px] font-medium">{{ content.content.title }}</p>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </CarouselItem>
-            </CarouselContent>
-            <CarouselNext class="h-16 z-20" />
-            <CarouselPrevious class="h-16 z-20" />
-        </Carousel>
+            <div class="absolute z-10 mb-4 bottom-0 left-0 w-full text-center">
+              <p class="text-[10px] font-medium">{{ content.title }}</p>
+            </div>
+          </CardContent>
+        </Card>
+      </CarouselItem>
+    </CarouselContent>
+
+    <CarouselNext class="h-16 z-20" />
+    <CarouselPrevious class="h-16 z-20" />
+  </Carousel>
 </template>
