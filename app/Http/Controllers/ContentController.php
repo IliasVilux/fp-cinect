96dc34d08caad4cb56ab0904c1e347b961476078
IliasVilux <iliasotsman@gmail.com>
2025-06-11 11:31:26 +0200

    feat: :construction: Add Detail View structure

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -10,9 +10,17 @@ class ContentController extends Controller
 {
     public function show(int $id)
     {
-        $content = Content::findOrFail($id);
+        $content = Content::with(['category', 'seasons.episodes'])->findOrFail($id);
+        $relatedContents = Content::where('category_id', $content->category_id)
+            ->where('id', '!=', $content->id)
+            ->where('type', $content->type)
+            ->inRandomOrder()
+            ->take(10)
+            ->get();
+
         return Inertia::render('content/Detail', [
             'content' => $content,
+            'relatedContents' => $relatedContents,
         ]);
     }
 }
--- resources/js/pages/content/Detail.vue
+++ resources/js/pages/content/Detail.vue
@@ -1,22 +1,129 @@
 <script setup lang="ts">
 import AppFooter from '@/components/AppFooter.vue';
 import ContentCarousel from '@/components/ContentCarousel.vue';
-import TextLink from '@/components/TextLink.vue';
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+import { Separator } from '@/components/ui/separator';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
-import { Content } from '@/types/models';
-import { Head, Link } from '@inertiajs/vue3';
+import { Content, Season } from '@/types/models';
+import { Head } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 defineProps<{
     content: Content;
+    relatedContents: Content[];
 }>();
+
+const selectedSeason = ref<Season | null>(null);
+
+const hoveredItemId = ref<string | null>(null);
+const setHoveredItem = (id: string) => {
+    hoveredItemId.value = id;
+};
+const clearHoveredItem = () => {
+    hoveredItemId.value = null;
+};
 </script>
 
 <template>
     <Head title="Dashboard" />
     <AppLayout layout="header">
-        {{ content }}
+        <section class="mt-20 flex flex-col gap-4 px-2 md:flex-row md:gap-10 lg:px-0">
+            <div class="w-full px-10 md:max-w-96 md:px-0">
+                <div class="aspect-[6/8] w-full">
+                    <img src="/images/welcome/hero-background.webp" :alt="content.title" class="size-full rounded-lg object-cover" />
+                </div>
+            </div>
+
+            <div class="w-full">
+                <h2 class="mb-2 text-xl font-bold uppercase md:text-3xl">{{ content.title }}</h2>
+                <div class="mb-8 flex h-5 items-center space-x-2 text-sm text-neutral-400">
+                    <p v-if="content.release_year">{{ content.release_year }}</p>
+                    <Separator v-if="content.release_year" orientation="vertical" />
+                    <p v-if="content.type == 'movie'">{{ content.duration }}</p>
+                    <p v-else>{{ content.seasons.length > 0 ? content.seasons.length : 0 }} seasons</p>
+                    <Separator orientation="vertical" />
+                    <p>{{ content.category.name }}</p>
+                </div>
+
+                <Tabs class="w-full" defaultValue="overview" as="section">
+                    <TabsList>
+                        <TabsTrigger value="overview">Overview</TabsTrigger>
+                        <TabsTrigger v-if="content.type != 'movie' && content.seasons.length > 0" value="seasons"> Seasons</TabsTrigger>
+                        <TabsTrigger value="trailer">Trailer</TabsTrigger>
+                    </TabsList>
+
+                    <Separator orientation="horizontal" class="mb-4" />
+
+                    <TabsContent value="overview">
+                        <p>{{ content.description }}</p>
+                    </TabsContent>
+
+                    <TabsContent value="seasons">
+                        <Carousel :opts="{ align: 'center' }" v-slot="{ canScrollPrev, canScrollNext }" class="relative w-full">
+                            <CarouselContent>
+                                <CarouselItem
+                                    v-for="season in content.seasons"
+                                    :key="season.id"
+                                    @click="selectedSeason = selectedSeason?.id === season.id ? null : season"
+                                    class="group basis-1/2 md:basis-1/4 pl-2 cursor-pointer"
+                                >
+                                    <Card
+                                        class="flex h-full min-h-20 justify-end rounded-lg px-3 py-1 transition duration-200 group-hover:border-indigo-600 group-hover:bg-[#120e28]"
+                                        :class="{
+                                            'border-indigo-600': selectedSeason && selectedSeason?.id === season.id,
+                                        }"
+                                    >
+                                        <CardContent class="p-0">
+                                            <p class="text-xs font-light text-neutral-300">S{{ season.season_number }}</p>
+                                            <p class="text-sm">{{ season.title || `Season ${season.season_number}` }}</p>
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                            </CarouselContent>
+
+                            <CarouselNext v-if="canScrollNext" class="z-20" />
+                            <CarouselPrevious v-if="canScrollPrev" class="z-20" />
+                        </Carousel>
+
+                        <section class="grid grid-cols-1 gap-2 md:grid-cols-3">
+                            <div v-for="episode in selectedSeason?.episodes" :key="episode.id">
+                                <div>
+                                    <p class="text-xs font-light text-neutral-300">Ep{{ episode.episode_number }}</p>
+                                    <p>{{ episode.title }}</p>
+                                    <p class="text-end text-xs text-neutral-400 md:text-start">
+                                        Duration: {{ episode.duration ? `${episode.duration} mins` : 'N/S' }}
+                                    </p>
+                                </div>
+                                <Separator orientation="horizontal" class="my-2 md:hidden" />
+                            </div>
+                        </section>
+                    </TabsContent>
+                    <TabsContent value="trailer">
+                        <p>Trailer</p>
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </section>
+
+        <section class="mt-20 border-sidebar-border/70 relative h-56 w-full rounded-xl border px-2 lg:px-0">
+            <PlaceholderPattern />
+        </section>
+
+        <section v-if="relatedContents.length > 0" class="mt-20">
+            <div class="mb-2 flex items-baseline justify-between px-2 lg:px-0">
+                <h2 class="z-10 text-xl font-semibold tracking-tight">More like this</h2>
+            </div>
+            <ContentCarousel
+                :contents="relatedContents"
+                :hoveredItemId="hoveredItemId"
+                :setHoveredItem="setHoveredItem"
+                :clearHoveredItem="clearHoveredItem"
+            />
+        </section>
+
         <AppFooter />
     </AppLayout>
 </template>
