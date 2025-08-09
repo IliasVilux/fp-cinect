b19f2717f3588799491bf7703f9be595781adf8d
IliasVilux <iliasotsman@gmail.com>
2025-08-10 00:22:01 +0200

    refactor: :recycle: Refactor 'DashboardCategory' page and add 'FeaturedHero'

---
+++ resources/js/components/dashboard-category/FeaturedHero.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Content } from '@/types/models';
+import { Link } from '@inertiajs/vue3';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+defineProps<{
+    featuredContent: Content;
+}>();
+</script>
+
+<template>
+    <section class="relative w-full aspect-[9/16] md:aspect-video">
+        <!-- Background image -->
+        <img
+            :src="featuredContent.cover_image || '/images/welcome/hero-background.webp'"
+            alt="Hero Image"
+            class="absolute inset-0 size-full object-cover z-0 mask-b-from-15% mask-b-to-85% lg:mask-l-from-30% lg:mask-l-to-95% lg:mask-b-from-55% lg:mask-b-to-100%"
+        />
+
+        <!-- Content -->
+        <div class="absolute inset-0 z-10 flex flex-col justify-end px-2 lg:-mt-40 lg:justify-center lg:px-4">
+            <h1 class="mt-2 max-w-xl text-3xl font-bold lg:text-6xl">{{ featuredContent.title }}</h1>
+            <p class="my-3 max-w-xl text-lg font-light lg:mt-7 lg:mb-5">{{ featuredContent.description }}</p>
+            <Link
+                :href="route('login')"
+                class="w-fit rounded-full px-5 py-1.5 bg-neutral-50 text-neutral-900 leading-normal hover:bg-neutral-200"
+            >
+                {{ t('dashboard-content.watchMore') }}
+            </Link>
+        </div>
+
+        <!-- Category Tag -->
+        <span class="absolute top-20 right-0 z-10 border-l-4 border-white bg-black/50 px-6 py-2 text-xs font-bold uppercase">{{
+            featuredContent.category.name
+        }}</span>
+    </section>
+</template>
--- resources/js/pages/dashboard/DashboardCategory.vue
+++ resources/js/pages/dashboard/DashboardCategory.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import AppFooter from '@/components/AppFooter.vue';
 import ContentCarousel from '@/components/ContentCarousel.vue';
+import FeaturedHero from '@/components/dashboard-category/FeaturedHero.vue';
 import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { BreadcrumbItem } from '@/types';
 import { Category, Content } from '@/types/models';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
@@ -36,39 +37,11 @@ const clearHoveredItem = () => {
 <template>
     <Head :title="`${contentType} Dashboard`" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-10 rounded-xl">
-            <!-- HERO -->
-            <section class="relative aspect-[9/16] w-full md:aspect-video">
-                <!-- Background image -->
-                <img
-                    :src="featuredContent.cover_image || '/images/welcome/hero-background.webp'"
-                    alt="Hero Image"
-                    class="absolute inset-0 z-0 size-full mask-b-from-15% mask-b-to-85% object-cover lg:mask-b-from-55% lg:mask-b-to-100% lg:mask-l-from-30% lg:mask-l-to-95%"
-                />
-
-                <!-- Content -->
-                <div class="absolute inset-0 z-10 flex flex-col justify-end px-2 lg:-mt-40 lg:justify-center lg:px-4">
-                    <h1 class="mt-2 max-w-xl text-3xl font-bold lg:text-6xl">{{ featuredContent.title }}</h1>
-                    <p class="my-3 max-w-xl text-lg font-light lg:mt-7 lg:mb-5">{{ featuredContent.description }}</p>
-                    <Link
-                        :href="route('login')"
-                        class="w-fit rounded-full bg-neutral-50 px-5 py-1.5 leading-normal text-neutral-900 hover:bg-neutral-200"
-                    >
-                        {{ t('dashboard-content.watchMore') }}
-                    </Link>
-                </div>
-
-                <!-- Category Tag -->
-                <span class="absolute top-20 right-0 z-10 border-l-4 border-white bg-black/50 px-6 py-2 text-xs font-bold uppercase">{{
-                    featuredContent.category.name
-                }}</span>
-            </section>
+            <FeaturedHero :featuredContent="featuredContent" />
 
             <!-- TRENDING -->
-            <section class="z-10 lg:-mt-40">
-                <div class="mb-2 flex items-baseline justify-between px-2 lg:px-4">
-                    <h2 class="z-10 text-xl font-semibold tracking-tight">{{ t('dashboard-content.trending') }}</h2>
-                </div>
+            <section class="z-10 mb-6 lg:-mt-40">
+                    <h2 class="text-xl font-semibold tracking-tight">{{ t('dashboard-content.trending') }}</h2>
                 <ContentCarousel
                     :contents="trendingContents"
                     :hoveredItemId="hoveredItemId"
@@ -78,10 +51,8 @@ const clearHoveredItem = () => {
             </section>
 
             <!-- TOP 10 -->
-            <section>
-                <div class="mb-2 flex items-baseline justify-between px-2 lg:px-4">
-                    <h2 class="z-10 text-xl font-semibold tracking-tight">{{ t('dashboard-content.topTen') }}</h2>
-                </div>
+            <section class="my-6">
+                <h2 class="text-xl font-semibold tracking-tight">{{ t('dashboard-content.topTen') }}</h2>
                 <ContentCarousel
                     :contents="topTen"
                     :hoveredItemId="hoveredItemId"
@@ -91,10 +62,8 @@ const clearHoveredItem = () => {
             </section>
 
             <!-- FOR YOU -->
-            <section>
-                <div class="mb-2 flex items-baseline justify-between px-2 lg:px-4">
-                    <h2 class="z-10 text-xl font-semibold tracking-tight">{{ t('dashboard-content.forYou') }}</h2>
-                </div>
+            <section class="my-6">
+                <h2 class="text-xl font-semibold tracking-tight">{{ t('dashboard-content.forYou') }}</h2>
                 <div class="grid grid-cols-2 gap-2 px-2 md:grid-cols-4 lg:grid-cols-6 lg:px-4">
                     <div
                         v-for="(item, index) in 6"
@@ -109,10 +78,8 @@ const clearHoveredItem = () => {
             </section>
 
             <!-- CATEGORIES CAROUSEL -->
-            <section v-for="(group, index) in contentsGroupedByCategory" :key="index">
-                <div class="mb-2 flex items-baseline justify-between px-2 lg:px-4">
-                    <h2 class="z-10 text-xl font-semibold tracking-tight">{{ group.category.name }}</h2>
-                </div>
+            <section v-for="(group, index) in contentsGroupedByCategory" :key="index" class="my-6">
+                <h2 class="text-xl font-semibold tracking-tight">{{ group.category.name }}</h2>
                 <ContentCarousel
                     :contents="group.contents"
                     :hoveredItemId="hoveredItemId"
@@ -120,7 +87,7 @@ const clearHoveredItem = () => {
                     :clearHoveredItem="clearHoveredItem"
                 />
             </section>
+
             <AppFooter />
-        </div>
     </AppLayout>
 </template>
