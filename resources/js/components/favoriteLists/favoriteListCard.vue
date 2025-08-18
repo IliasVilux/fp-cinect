1e0a30ca76632c8c287583ec73c6408ab54960d9
IliasVilux <iliasotsman@gmail.com>
2025-08-18 13:20:10 +0200

    feat: :construction: Initial structure in 'favoriteListCard' component

--- resources/js/components/favoriteLists/favoriteListCard.vue
+++ resources/js/components/favoriteLists/favoriteListCard.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { FavoriteList } from '@/types/models';
 import FavoriteListDialog from '@/components/favoriteLists/favoriteListDialog.vue';
+import { Eye, EyeClosed } from 'lucide-vue-next';
+import ContentCard from '@/components/ContentCard.vue';
 
 defineProps<{
     list: FavoriteList;
@@ -8,5 +10,12 @@ defineProps<{
 </script>
 
 <template>
-    <FavoriteListDialog :list="list" />
+    <div>
+        <p>{{ list.name }}</p>
+        <p>{{ list.description }}</p>
+        <Eye v-if="list.is_public" />
+        <EyeClosed v-else />
+        <ContentCard v-for="content in list.contents" :key="content.id" :content="content" />
+        <FavoriteListDialog :list="list" />
+    </div>
 </template>
