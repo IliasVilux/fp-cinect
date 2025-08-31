6bc133888973670b13bb6d2fdb79d3bfe520cdac
IliasVilux <iliasotsman@gmail.com>
2025-08-31 20:30:15 +0200

    refactor: :art: Refactor 'ReviewList' component

--- resources/js/components/detail/ReviewList.vue
+++ resources/js/components/detail/ReviewList.vue
@@ -27,27 +27,27 @@ defineProps<{
 
 <template>
     <section>
-        <Heading :title="t('detail.reviewCount', { count: reviews?.length })" />
+        <Heading :title="t('detail.reviewCount', { count: reviews?.length })" class="ml-5 lg:ml-0 mb-5" />
 
-        <div class="flex flex-col gap-y-0">
-            <div v-for="review in reviews" :key="review.id" class="flex gap-x-0">
+        <div class="flex flex-col gap-y-5 px-5 lg:px-0">
+            <div v-for="review in reviews" :key="review.id" class="flex gap-x-3">
                 <!-- Avatar -->
                 <Avatar class="size-8 overflow-hidden rounded-full">
                     <AvatarImage v-if="review.user.avatar" :src="review.user.avatar" :alt="review.user.name" />
-                    <AvatarFallback class="rounded-lg bg-neutral-200 font-semibold text-black dark:bg-neutral-700 dark:text-white">
+                    <AvatarFallback class="rounded-lg bg-gradient-to-tr from-neutral-700 to-neutral-800 text-neutral-50 font-semibold">
                         {{ getInitials(review.user?.name) }}
                     </AvatarFallback>
                 </Avatar>
 
-                <div class="flex w-full flex-col gap-y-0">
+                <div class="flex w-full flex-col gap-y-1">
                     <div class="flex items-baseline gap-x-2">
                         <p class="text-sm font-semibold">@{{ review.user.name }}</p>
                         <p class="text-muted-foreground text-xs">{{ dayjs(review.created_at).fromNow() }}</p>
                     </div>
                     <div class="flex w-full items-start justify-between">
                         <p class="text-pretty">{{ review.review_text }}</p>
-                        <Link :href="route('reviews.destroy', review.id)" method="delete" v-if="review.user.id === user.id">
-                            <Trash2 class="aspect-square size-full cursor-pointer" />
+                        <Link :href="route('reviews.destroy', review.id)" method="delete" class="ml-3" v-if="review.user.id === user.id">
+                            <Trash2 class="aspect-square size-5 cursor-pointer hover:text-red-600 transition-colors duration-300" />
                         </Link>
                     </div>
                 </div>
