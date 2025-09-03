a90d9fd809b301ebf987a2752dfa1c3ea267ce1a
IliasVilux <iliasotsman@gmail.com>
2025-09-04 00:04:57 +0200

    refactor: :art: Refactor 'EpisodeList' component

--- resources/js/components/detail/EpisodeList.vue
+++ resources/js/components/detail/EpisodeList.vue
@@ -10,15 +10,20 @@ defineProps<{
 </script>
 
 <template>
-    <section class="grid grid-cols-1 gap-2 md:grid-cols-3">
-        <div v-for="episode in selectedSeason?.episodes" :key="episode.id">
-            <div>
-                <p class="text-xs font-light text-neutral-300">Ep{{ episode.episode_number }}</p>
+    <section class="grid grid-cols-1 gap-2">
+        <div
+            v-for="episode in selectedSeason?.episodes"
+            :key="episode.id"
+            class="border-border flex flex-col rounded-md border bg-gradient-to-l from-neutral-950/80 to-neutral-950/0 p-4 transition-colors duration-300 hover:border-indigo-600 hover:bg-neutral-800 md:flex-row md:items-center md:justify-between"
+        >
+            <div class="flex flex-col gap-1 md:flex-row md:items-center md:gap-4">
+                <p class="text-xs font-light text-neutral-600 md:text-neutral-300">{{ episode.episode_number }}</p>
                 <p>{{ episode.title }}</p>
-                <p class="text-end text-xs text-neutral-400 md:text-start">
-                    {{ t('detail.duration') }}: {{ episode.duration ? `${episode.duration} mins` : 'N/S' }}
-                </p>
             </div>
+
+            <p class="mt-2 text-end text-xs text-neutral-400 md:mt-0 md:text-start">
+                {{ t('detail.duration') }}: {{ episode.duration ? `${episode.duration} mins` : 'N/S' }}
+            </p>
         </div>
     </section>
 </template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -18,7 +18,7 @@ withDefaults(defineProps<Props>(), {
     <AppSidebarLayout v-if="layout === 'sidebar'" :breadcrumbs="breadcrumbs">
         <slot />
     </AppSidebarLayout>
-    <AppHeaderLayout v-else :breadcrumbs="breadcrumbs" class="mx-auto w-full max-w-6xl">
+    <AppHeaderLayout v-else :breadcrumbs="breadcrumbs" class="mx-auto w-full">
         <slot />
     </AppHeaderLayout>
 </template>
