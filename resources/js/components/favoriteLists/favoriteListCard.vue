5005d543e501cf19ffefd3566bc975dc56f512fe
IliasVilux <iliasotsman@gmail.com>
2025-09-15 18:39:33 +0200

    feat: :sparkles: Implement add card in empty 'favoriteListCard'

--- resources/js/components/favoriteLists/favoriteListCard.vue
+++ resources/js/components/favoriteLists/favoriteListCard.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import Heading from '@/components/common/Heading.vue';
 import FavoriteListDialog from '@/components/favoriteLists/favoriteListDialog.vue';
-import { FavoriteList } from '@/types/models';
 import { Badge } from '@/components/ui/badge';
-import ContentCarousel from '../common/ContentCarousel.vue';
+import { FavoriteList } from '@/types/models';
 import { Link } from '@inertiajs/vue3';
+import { Plus } from 'lucide-vue-next';
+import ContentCarousel from '../common/ContentCarousel.vue';
 
 defineProps<{
     list: FavoriteList;
@@ -12,7 +13,7 @@ defineProps<{
 </script>
 
 <template>
-    <div class="relative flex h-full flex-col rounded-xl bg-gradient-to-br from-black to-neutral-950/10 border border-border">
+    <div class="border-border relative flex h-full flex-col rounded-xl border bg-gradient-to-br from-black to-neutral-950/10">
         <div class="flex-1">
             <div class="flex items-center justify-between gap-2 px-4 pt-4">
                 <Heading :title="list.name" />
@@ -20,14 +21,21 @@ defineProps<{
                 <Badge v-else variant="outline">Privada</Badge>
             </div>
 
-            <p class="text-pretty px-4">{{ list.description }}</p>
+            <p class="px-4 text-pretty">{{ list.description }}</p>
 
-            <ContentCarousel v-if="list.contents" :contents="list.contents" class="mt-4" basis="basis-[calc(100%/1.5)] md:basis-[calc(100%/2.5)] lg:basis-[calc(100%/3.5)]" />
-            <div v-else>
-                <Link :href="route('favorite-lists.show', list.id)" class="flex h-48 items-center justify-center rounded-b-xl bg-neutral-900 text-center hover:bg-neutral-800">
-                    <p class="text-pretty">No contents yet. Click to view and add some!</p>
-                </Link>
-            </div>
+            <ContentCarousel
+                v-if="list.contents && list.contents.length > 0"
+                :contents="list.contents"
+                class="mt-4"
+                basis="basis-[calc(100%/1.5)] md:basis-[calc(100%/2.5)] lg:basis-[calc(100%/3.5)]"
+            />
+            <Link v-else :href="route('contents.explore')">
+                <div
+                    class="group relative mt-6 ml-4 flex aspect-[5/7] h-48 items-center justify-center overflow-hidden rounded-md bg-gradient-to-b from-neutral-950 to-neutral-900"
+                >
+                    <component :is="Plus" class="size-16 text-neutral-600 transition-colors duration-200 group-hover:text-indigo-600" />
+                </div>
+            </Link>
         </div>
 
         <FavoriteListDialog :list="list" class="justify-end p-4" />
