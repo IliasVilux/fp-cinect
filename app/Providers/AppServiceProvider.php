23c0cadbb669381df92f10eebad1e66eeafbd5a9
IliasVilux <iliasotsman@gmail.com>
2025-10-29 18:06:18 +0100

    Force https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Inertia::share('locale', function () {
             return app()->getLocale();
         });
